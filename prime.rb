146ccf2fe985f8091d3e1e672a68274d591835ab
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-04 23:41:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@ require 'pry'
 def prime?(int)
   return true if int = 2
   possibilities = Array (2..int)
+  binding.pry
   possibilities.each do |possibility|
     return true if possibility * possibility == int
   end
