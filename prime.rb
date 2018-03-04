45cf44c02fd355f4c32a3e9ae5c9703b585f4189
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-04 23:50:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def prime?(int)
-  # return false if int < 2
+  return false if int < 2
   possibilities = Array (2..int-1)
   possibilities.each do |possibility|
     return false if int % possibility == 0
